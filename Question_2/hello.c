30293d998fbf8c1641c68079efb176d649bf774c
Robert <wruoting@gmail.com>
2020-02-23 13:21:08 -0500

    Question 2

---
+++ Question_2/hello.c
@@ -0,0 +1,9 @@
+/* Simple Linux Kernel display in user mode*/
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello from Ruoting Wang!\n");
+	return 0;
+}
+
---
+++ Question_2/hello.cpp
@@ -0,0 +1,9 @@
+/* Simple Linux Kernel display in user mode in cpp using cout*/
+#include <iostream>
+
+int main(void)
+{
+	std::cout << "Hello from Ruoting Wang!\n";
+	return 0;
+}
+
